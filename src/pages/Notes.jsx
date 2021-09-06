18d3fba67fa8202c1acd7525fce790051b72e7b9
Paul Park <paulpark330@hotmail.com>
2021-09-06 00:12:55 -0700

    on lesson 12

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Container from '@material-ui/core/Container'
+import NoteCard from '../components/NoteCard'
 
 export default function Notes() {
   const [notes, setNotes] = useState([])
@@ -8,11 +12,24 @@ export default function Notes() {
     .then(data => setNotes(data))
   }, [])
 
+  const handleDelete = async (id) => {
+    await fetch('http://localhost:8000/notes/' + id, {
+      method: 'DELETE'
+    })
+
+    const newNotes = notes.filter(note => note.id !== id)
+    setNotes(newNotes)
+  }
+
   return (
-    <div>
-      {notes.map(note => (
-        <p key={note.id}>{ note.title }</p>
-      ))}
-    </div>
-  )
+    <Container>
+      <Grid container spacing={3}>
+        {notes.map((note) => (
+          <Grid item key={note.id} xs={12} md={6} lg={4}>
+            <NoteCard note={note} handleDelete={handleDelete} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
