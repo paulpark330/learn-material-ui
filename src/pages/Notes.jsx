935a5786ec0c86d0cd42ca7ad27165d2f5b588e3
Paul Park <paulpark330@hotmail.com>
2021-09-06 15:16:20 -0700

    compeleted learn-material-ui

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,34 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import Grid from '@material-ui/core/Grid'
-import Container from '@material-ui/core/Container'
-import NoteCard from '../components/NoteCard'
+/** @format */
+
+import React, { useEffect, useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import NoteCard from '../components/NoteCard';
+import Masonry from 'react-masonry-css';
 
 export default function Notes() {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState([]);
   useEffect(() => {
     fetch('http://localhost:8000/notes')
-    .then(res => res.json())
-    .then(data => setNotes(data))
-  }, [])
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }, []);
 
   const handleDelete = async (id) => {
     await fetch('http://localhost:8000/notes/' + id, {
-      method: 'DELETE'
-    })
+      method: 'DELETE',
+    });
 
-    const newNotes = notes.filter(note => note.id !== id)
-    setNotes(newNotes)
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1
   }
 
   return (
     <Container>
-      <Grid container spacing={3}>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
         {notes.map((note) => (
-          <Grid item key={note.id} xs={12} md={6} lg={4}>
+          <div key={note.id}>
             <NoteCard note={note} handleDelete={handleDelete} />
-          </Grid>
+          </div>
         ))}
-      </Grid>
+      </Masonry>
     </Container>
   );
 }
