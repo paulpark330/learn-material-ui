059b75e6acccf79fd95d0eb702efe639ba2a6036
Paul Park <paulpark330@hotmail.com>
2021-09-06 00:47:51 -0700

    on lesson 15

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Notes from './pages/Notes';
 import Create from './pages/Create';
 import { createTheme, ThemeProvider } from '@material-ui/core';
 import { purple } from '@material-ui/core/colors';
+import Layout from './components/Layout';
 
 const theme = createTheme({
   palette: {
@@ -26,14 +27,16 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <Notes />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-        </Switch>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <Notes />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </Layout>
       </Router>
     </ThemeProvider>
   );
